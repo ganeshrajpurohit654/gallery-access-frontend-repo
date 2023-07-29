f676d026678a77701094262e7079720664052cca
ganeshrajpurohit654 <85803722+ganeshrajpurohit654@users.noreply.github.com>
2023-07-30 04:28:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Import required packages
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
@@ -6,18 +7,39 @@ const port = process.env.PORT || 3000;
 // Parse JSON bodies
 app.use(express.json());
 
+// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
+const botToken = '6175274258:AAFBQL_euNYLqoye5Hrcgb_t6cTHemXycI8';
+
 // POST request to handle gallery access request
 app.post('/gallery-access', async (req, res) => {
   try {
-    // Perform any necessary processing here
-    // For example, you can interact with your Telegram bot and retrieve gallery photos
+    // Get user's chat ID and other information from the request body
+    const chatId = req.body.chat_id; // This should be the chat ID of your Telegram bot's user
+    // ...
+
+    // Interact with your Telegram bot to request gallery access and retrieve photos
+    // Use the Telegram Bot API with the bot token and chat ID to send messages and receive responses
+    // For example, you can use the 'sendMessage' method to request gallery access:
+    const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: 'Please grant gallery access to proceed.',
+      }),
+    });
+
+    // Handle the response from the Telegram Bot API, such as checking if the message was sent successfully
+    // ...
 
     // Simulate a response for demonstration purposes
-    // In a real implementation, you'll handle Telegram bot interaction and gallery photo retrieval
-    const response = { status: 'success', message: 'Gallery access granted' };
+    // In a real implementation, you'll handle the Telegram bot interaction and photo retrieval
+    const apiResponse = await response.json();
 
     // Respond to the client
-    res.json(response);
+    res.json(apiResponse);
   } catch (error) {
     console.error('Error handling gallery access request:', error);
     res.status(500).json({ status: 'error', message: 'Failed to handle gallery access request' });
